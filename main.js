6cbdb833e8efa6bfd075c968a1c45d3f9b401bf3
Cleber Moraes <alvesmoraesb@gmail.com>
2023-02-21 21:06:16 -0300

    Criando função para carregar a pagina com valores

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 let hours = document.querySelectorAll(".hours");
 let lastTime = document.querySelectorAll(".last-time");
 
-
-function daily() {
-
+function start() {
   fetch("./data.json").then((response) => {
     response.json().then((dados) => {
 
@@ -19,54 +17,77 @@ function daily() {
         document.getElementById('monthly').style.color = 'hsl(235, 45%, 61%)';
 
       });
-
     });
-
-  })
+  });
 }
+start();
 
-function weekly() {
 
-  fetch("./data.json").then((response) => {
-    response.json().then((dados) => {
+  function daily() {
 
-      console.log(dados.usuarios[0].timeframes.monthly.current);
+    fetch("./data.json").then((response) => {
+      response.json().then((dados) => {
 
-      hours.forEach((e, i) => {
+        console.log(dados.usuarios[0].timeframes.daily.current);
 
-        hours[i].innerHTML = dados.usuarios[i].timeframes.weekly.current + " hrs ";
-        lastTime[i].innerHTML = " Last Daily " + dados.usuarios[i].timeframes.weekly.previous + "hrs";
+        hours.forEach((e, i) => {
 
-        document.getElementById('daily').style.color = 'hsl(235, 45%, 61%)';
-        document.getElementById('weekly').style.color = 'hsl(236, 100%, 87%)';
-        document.getElementById('monthly').style.color = 'hsl(235, 45%, 61%)';
+          hours[i].innerHTML = dados.usuarios[i].timeframes.daily.current + " hrs ";
+          lastTime[i].innerHTML = " Last Daily " + dados.usuarios[i].timeframes.daily.previous + "hrs";
+
+          document.getElementById('daily').style.color = 'hsl(236, 100%, 87%)';
+          document.getElementById('weekly').style.color = 'hsl(235, 45%, 61%)';
+          document.getElementById('monthly').style.color = 'hsl(235, 45%, 61%)';
+
+        });
 
       });
 
-    });
+    })
+  }
 
-  })
-}
+  function weekly() {
 
-function monthly() {
+    fetch("./data.json").then((response) => {
+      response.json().then((dados) => {
 
-  fetch("./data.json").then((response) => {
-    response.json().then((dados) => {
+        console.log(dados.usuarios[0].timeframes.monthly.current);
 
-      console.log(dados.usuarios[0].timeframes.monthly.current);
+        hours.forEach((e, i) => {
 
-      hours.forEach((e, i) => {
+          hours[i].innerHTML = dados.usuarios[i].timeframes.weekly.current + " hrs ";
+          lastTime[i].innerHTML = " Last Daily " + dados.usuarios[i].timeframes.weekly.previous + "hrs";
 
-        hours[i].innerHTML = dados.usuarios[i].timeframes.monthly.current + " hrs ";
-        lastTime[i].innerHTML = " Last Daily " + dados.usuarios[i].timeframes.monthly.previous + "hrs";
+          document.getElementById('daily').style.color = 'hsl(235, 45%, 61%)';
+          document.getElementById('weekly').style.color = 'hsl(236, 100%, 87%)';
+          document.getElementById('monthly').style.color = 'hsl(235, 45%, 61%)';
 
-        document.getElementById('daily').style.color = 'hsl(235, 45%, 61%)';
-        document.getElementById('weekly').style.color = 'hsl(235, 45%, 61%)';
-        document.getElementById('monthly').style.color = 'hsl(236, 100%, 87%)';
+        });
 
       });
 
-    });
+    })
+  }
 
-  })
-}
+  function monthly() {
+
+    fetch("./data.json").then((response) => {
+      response.json().then((dados) => {
+
+        console.log(dados.usuarios[0].timeframes.monthly.current);
+
+        hours.forEach((e, i) => {
+
+          hours[i].innerHTML = dados.usuarios[i].timeframes.monthly.current + " hrs ";
+          lastTime[i].innerHTML = " Last Daily " + dados.usuarios[i].timeframes.monthly.previous + "hrs";
+
+          document.getElementById('daily').style.color = 'hsl(235, 45%, 61%)';
+          document.getElementById('weekly').style.color = 'hsl(235, 45%, 61%)';
+          document.getElementById('monthly').style.color = 'hsl(236, 100%, 87%)';
+
+        });
+
+      });
+
+    })
+  }
